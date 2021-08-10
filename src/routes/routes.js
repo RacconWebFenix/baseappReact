7d95cfad4692ec6ccda2a27eeb763021e4e6dd17
Marcos Domingues <dfenixweb@gmail.com>
2021-08-10 10:19:08 -0300

    teste

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,22 +3,20 @@ import { Route, BrowserRouter } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Sobre from "../pages/Sobre";
-import Personagens from "../pages/Skycraper";
+import Skycraper from "../pages/Skycraper";
 import Contato from "../pages/Contato";
-import Header from "../components/Header"
+import Header from "../components/Header";
 
 const Routes = () => {
-    return (
-     
-      <BrowserRouter>
-        <Header />
-        <Route component={Home} path="/" exact />
-        <Route component={Sobre} path="/sobre" />
-        <Route component={Personagens} path="/skycraper" />
-        <Route component={Contato} path="/contato" />
-      </BrowserRouter>
-    );
-  };
-  
-  export default Routes;
-  
+  return (
+    <BrowserRouter>
+      <Header />
+      <Route component={Home} path="/" exact />
+      <Route component={Sobre} path="/sobre" />
+      <Route component={Skycraper} path="/skycraper" />
+      <Route component={Contato} path="/contato" />
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
